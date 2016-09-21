742c0b85a521852c78397e6177c4f78cd2744174
Jack Hou <jackhou@Jacks-MacBook-Pro.local>
2016-09-21 14:25:42 -0400

    Implements more ButterKnife

--- app/src/main/java/com/example/jackhou/paint/MainActivity.java
+++ app/src/main/java/com/example/jackhou/paint/MainActivity.java
@@ -2,14 +2,14 @@ package com.example.jackhou.paint;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemSelected;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,56 +27,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                paintView.clear(currentStrokeSize,currentColor);
-            }
-        });
-
-        eraser.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                paintView.erase();
-            }
-        });
-
-        ArrayAdapter<CharSequence> stroke_adapter = ArrayAdapter.createFromResource(this, R.array.stroke_size, android.R.layout.simple_spinner_item);
-        stroke_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        strokeSize.setAdapter(stroke_adapter);
-        strokeSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-                currentStrokeSize = pos+1;
-                paintView.updateStroke(currentStrokeSize, currentColor);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent){
-
-            }
-        });
-
-        ArrayAdapter<CharSequence> color_adapter = ArrayAdapter.createFromResource(this, R.array.color_options, android.R.layout.simple_spinner_item);
-        color_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        colorOption.setAdapter(color_adapter);
-        colorOption.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-                currentColor = pos;
-                paintView.updateColor(currentStrokeSize, currentColor);
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent){
-
-            }
-        });
-
+        initAdapters();
 
     }
 
+    public void initAdapters(){
+        ArrayAdapter<CharSequence> strokeAdapter = ArrayAdapter.createFromResource(this, R.array.stroke_size, android.R.layout.simple_spinner_item);
+        strokeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        strokeSize.setAdapter(strokeAdapter);
 
+        ArrayAdapter<CharSequence> colorAdapter = ArrayAdapter.createFromResource(this, R.array.color_options, android.R.layout.simple_spinner_item);
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorOption.setAdapter(colorAdapter);
+    }
+
+    @OnClick(R.id.reset_button)
+    public void onResetClick(){
+        paintView.clear(currentStrokeSize,currentColor);
+    }
+
+
+    @OnClick(R.id.eraser)
+    public void onEraserClick(){
+        paintView.erase();
+    }
+
+    @OnItemSelected(R.id.color_spinner)
+    public void colorSpinnerItemSelected(Spinner spinner, int position){
+        currentColor = position;
+        paintView.updateColor(currentStrokeSize, currentColor);
+    }
+
+    @OnItemSelected(R.id.stroke_spinner)
+    public void strokeSpinnerItemSelected(Spinner spinner, int position){
+        currentStrokeSize = position+1;
+        paintView.updateStroke(currentStrokeSize, currentColor);
+    }
 }
